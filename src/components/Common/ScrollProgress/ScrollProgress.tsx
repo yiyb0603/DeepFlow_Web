8dbc94b151398df162f3f155279731a6172d67fa
yiyb0603 <yiyb0603@gmail.com>
2021-04-07 12:10:47 +0900

    Add: Post Scroll progress
    글 조회 페이지에 스크롤진행도바 추가

---
+++ src/components/Common/ScrollProgress/ScrollProgress.tsx
@@ -0,0 +1,61 @@
+import { useState, useCallback, useEffect, useRef, memo, MouseEvent } from 'react';
+import classNames from 'classnames';
+import { ClassNamesFn } from 'classnames/types';
+
+const style = require('./ScrollProgress.scss');
+const cx: ClassNamesFn = classNames.bind(style);
+
+const ScrollProgress = memo((): JSX.Element => {
+  const [width, setWidth] = useState<number>(0);
+  const progressRef = useRef<HTMLDivElement | null>(null);
+
+  const handleProgressMove = useCallback((e: MouseEvent<HTMLDivElement>): void => {
+    if (progressRef.current !== null) {
+      const { scrollWidth } = progressRef.current;
+      const { clientX } = e;
+
+      const selectedPercent: number = ((clientX / scrollWidth) * 100);
+      setWidth(selectedPercent);
+      
+      const { scrollHeight, clientHeight } = document.body;
+      const windowHeight: number = scrollHeight - clientHeight;
+
+      const moveScrollPercent: number = ((windowHeight * selectedPercent) / 100);
+
+      window.scrollTo({
+        top: moveScrollPercent,
+        behavior: 'smooth',
+      })
+    }
+  }, []);
+
+  const handleScroll = useCallback((): void => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop === 0) {
+      setWidth(0);
+      return;
+    }
+
+    const windowHeight: number = scrollHeight - clientHeight;
+    const currentPercent: number = (scrollTop / windowHeight);
+
+    setWidth(currentPercent * 100);
+  }, []);
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, true);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll, true);
+    }
+  }, [handleScroll]);
+
+  return (
+    <div className={cx('ScrollProgress')} ref={progressRef} onClick={handleProgressMove}>
+      <div className={cx('ScrollProgress-Progress')} style={{ width: width + '%' }} ></div>
+    </div>
+  );
+});
+
+export default ScrollProgress;
--- src/pages/Post/PostPage.tsx
+++ src/pages/Post/PostPage.tsx
@@ -1,9 +1,11 @@
 import PageTemplate from 'components/Template/PageTemplate';
+import ScrollProgress from 'components/Common/ScrollProgress';
 import PostContainer from 'containers/Post';
 
 const PostPage = (): JSX.Element => {
   return (
     <PageTemplate>
+      <ScrollProgress />
       <PostContainer />
     </PageTemplate>
   );
