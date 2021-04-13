03fc3b3b94f318bec21b0fe22131bce24376a80b
yiyb0603 <yiyb0603@gmail.com>
2021-04-13 15:24:41 +0900

    Fix: User Navbar 호버 수정
    기존의 Link 태그 스타일 제거

--- src/components/User/SelectNavbar/SelectNavbar.tsx
+++ src/components/User/SelectNavbar/SelectNavbar.tsx
@@ -1,13 +1,15 @@
 import { useMemo } from 'react';
 import classNames from 'classnames';
 import { ClassNamesFn } from 'classnames/types';
-import { Link, useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import { History } from 'history';
 
 const style = require('./SelectNavbar.scss');
 const cx: ClassNamesFn = classNames.bind(style);
 
 const SelectNavbar = (): JSX.Element => {
   const { pathname } = useLocation();
+  const history: History = useHistory();
   const splitedPath: string[] = useMemo(() => pathname.split('/'), [pathname]);
 
   const userIdx: number = useMemo(() => Number(splitedPath[2]), [splitedPath]);
@@ -15,23 +17,23 @@ const SelectNavbar = (): JSX.Element => {
 
   return (
     <div className={cx('SelectNavbar')}>
-      <Link
-        to={`/user/${userIdx}`}
+      <div
         className={cx('SelectNavbar-Item', {
           'SelectNavbar-Item-Current': userPath === 'user',
         })}
+        onClick={() => history.push(`/user/${userIdx}`)}
       >
         <div>프로필</div>
-      </Link>
+      </div>
       
-      <Link
-        to={`/user-recommand/${userIdx}`}
+      <div
         className={cx('SelectNavbar-Item', {
           'SelectNavbar-Item-Current': userPath === 'user-recommand',
         })}
+        onClick={() => history.push(`/user-recommand/${userIdx}`)}
       >
         <div>추천 목록</div>
-      </Link>
+      </div>
     </div>
   );
 };
