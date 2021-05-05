32f3487e53e9a1af2aad7861c803e77bb994dc3d
권용빈 <yiyb0603@gmail.com>
2021-05-05 16:59:27 +0900

    Add: FallbackLoader 컴포넌트 추가

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { ChakraProvider } from '@chakra-ui/react';
+import FallbackLoader from 'components/Common/FallbackLoader';
 import App from 'components/App';
 
 import 'styles/util.scss';
@@ -12,7 +13,7 @@ const Root = (): JSX.Element => {
   return (
     <RecoilRoot>
       <BrowserRouter>
-        <Suspense fallback={null}>
+        <Suspense fallback={<FallbackLoader />}>
           <ChakraProvider>
             <App />
           </ChakraProvider>
