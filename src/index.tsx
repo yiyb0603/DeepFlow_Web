948dd4849544e771b5471f00ce52198b382eb8d9
권용빈 <yiyb0603@gmail.com>
2021-06-20 13:38:05 +0900

    Add: Sentry init

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { SENTRY_DSN } from 'config/config.json';
 import Root from 'Root';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
